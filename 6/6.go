2f385aa1fe13eb13bec7b584764fa235c8062c85
nwunderly <48489521+nwunderly@users.noreply.github.com>
2020-10-27 19:58:42 -0400

    Problem 6 - Go

---
+++ 6/6.go
@@ -0,0 +1,23 @@
+
+/*
+Project Euler - Problem 6
+Sum square difference
+
+https://projecteuler.net/problem=6
+*/
+
+package main
+
+import "fmt"
+
+func main() {
+	sumSquares := 0
+	squareSum := 0
+	for i := 1; i <= 100; i++ {
+		sumSquares += i * i
+		squareSum += i
+	}
+	squareSum = squareSum * squareSum
+	difference := squareSum - sumSquares
+	fmt.Println(difference)
+}
