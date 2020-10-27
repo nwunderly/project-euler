e62424ef634b9b8b8db2b7c3f6dbb231aaf7c747
nwunderly <48489521+nwunderly@users.noreply.github.com>
2020-10-27 01:24:49 -0400

    Problem 4 - Go

---
+++ 4/4.go
@@ -0,0 +1,37 @@
+
+// Project Euler - Problem 4
+// Largest palindrome product
+
+package main
+
+import (
+	"fmt"
+	"strconv"
+)
+
+func is_palindrome(s string) bool {
+	for i := 0; i < len(s); i++ {
+		if s[i] != s[len(s)-1-i] {
+			return false
+		}
+	}
+	return true
+}
+
+func main() {
+
+	var largest int
+
+	for i := 999; i > 100; i-- {
+		for j := 999; j > 100; j-- {
+			n := i * j
+			s := strconv.Itoa(n)
+			if is_palindrome(s) {
+				if n > largest {
+					largest = n
+				}
+			}
+		}
+	}
+	fmt.Println(largest)
+}
