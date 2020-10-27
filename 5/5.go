6534e82442fa35033f25aced6eed66c6c7575e4f
nwunderly <48489521+nwunderly@users.noreply.github.com>
2020-10-27 19:41:40 -0400

    Problem 5 - Go

---
+++ 5/5.go
@@ -0,0 +1,25 @@
+
+// Project Euler - Problem 5
+// Smallest multiple
+
+package main
+
+import "fmt"
+
+func makeDivisibleBy(number int, factor int) int {
+	for i := 1; i <= factor; i++ {
+		if (number * i) % factor == 0 {
+			return i
+		}
+	}
+	return 0
+}
+
+func main() {
+	var n = 1
+	for i := 1; i <= 20; i++ {
+		n *= makeDivisibleBy(n, i)
+		fmt.Println(i, n)
+	}
+	fmt.Println(n)
+}
