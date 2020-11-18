ef414b29565b7b2c6130371c0711277173040181
nwunderly <48489521+nwunderly@users.noreply.github.com>
2020-11-18 00:05:33 -0500

    reorganize and create 11.go

---
+++ Go/11.go
@@ -0,0 +1,12 @@
+/*
+Project Euler - Problem 11
+Largest product in a grid
+
+https://projecteuler.net/problem=11
+*/
+
+package main
+
+func problem11() {
+	
+}
--- Go/main.go
+++ Go/main.go
@@ -5,35 +5,46 @@ package main
 import (
 	"fmt"
 	"os"
+	"strconv"
 )
 
-var funcMap = make(map[string]func())
-
-func registerFunction(key string, function func()) {
-	funcMap[key] = function
-}
 
 func main() {
 
 	fmt.Println(funcMap)
 
 	for _, arg := range os.Args[1:] {
-
-		if function, hasKey := funcMap[arg]; hasKey {
+		i, err := strconv.Atoi(arg)
+		if err != nil {
+			fmt.Printf("Invalid argument '%s'\n", arg)
+		} else if function, hasKey := funcMap[i]; hasKey {
 			fmt.Printf("Running function 'problem%s'\n", arg)
 			function()
 		} else {
-			fmt.Printf("Could not find function 'problem%s'\n", arg)
+			fmt.Printf("Could not find function '%s'\n", arg)
 		}
 	}
 	fmt.Println("Done.")
 }
 
-func init() {
-	registerFunction("1",  problem1 )
-	registerFunction("4",  problem4 )
-	registerFunction("5",  problem5 )
-	registerFunction("6",  problem6 )
-	registerFunction("9",  problem9 )
-	registerFunction("10", problem10)
+var funcMap = map[int]func(){
+	1  : problem1,
+	//2  : problem2,
+	//3  : problem3,
+	4  : problem4,
+	5  : problem5,
+	6  : problem6,
+	//7  : problem7,
+	//8  : problem8,
+	9  : problem9,
+	10 : problem10,
+	11 : problem11,
+	//12 : problem12,
+	//13 : problem13,
+	//14 : problem14,
+	//15 : problem15,
+	//16 : problem16,
+	//17 : problem17,
+	//18 : problem18,
+	//19 : problem19,
 }
